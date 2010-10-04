a964e1c01d8fe38d9493773423c42e6d7375de21
D. DeZurik <damien@osilabs.com>
2010-10-03 23:28:00 -0500

    v0.2 - has icon

--- projects/twinCitiesTraffic/gen/com/osilabs/android/apps/R.java
+++ projects/twinCitiesTraffic/gen/com/osilabs/android/apps/R.java
@@ -11,7 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int ic_launcher_main=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
         public static final int mainWebView=0x7f050000;
