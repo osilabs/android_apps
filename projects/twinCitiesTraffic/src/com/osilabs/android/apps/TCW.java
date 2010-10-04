48054f505f8e853a7ce1d24bbd244636cd5957c8
D. DeZurik <damien@osilabs.com>
2010-10-03 21:32:21 -0500

    created initial TC Weather project

---
+++ projects/twinCitiesTraffic/src/com/osilabs/android/apps/TCW.java
@@ -0,0 +1,13 @@
+package com.osilabs.android.apps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TCW extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
