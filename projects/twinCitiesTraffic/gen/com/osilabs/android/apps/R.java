8b9acad90cdb02b7fab08b97126ccb649b9339e2
D. DeZurik <damien@osilabs.com>
2010-10-16 23:21:07 -0500

    TCT v0.1.6 - Quick View switcher under webview

--- projects/twinCitiesTraffic/gen/com/osilabs/android/apps/R.java
+++ projects/twinCitiesTraffic/gen/com/osilabs/android/apps/R.java
@@ -8,6 +8,9 @@
 package com.osilabs.android.apps;
 
 public final class R {
+    public static final class array {
+        public static final int view_choices=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -15,13 +18,17 @@ public final class R {
         public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int mainWebView=0x7f050000;
+        public static final int mainWebView=0x7f070000;
+        public static final int view_choice_spinner=0x7f070001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040000;
+        public static final int quick_view_label=0x7f040001;
+    }
+    public static final class style {
+        public static final int osistyle=0x7f060000;
     }
 }
--- projects/twinCitiesTraffic/src/com/osilabs/android/apps/TCT.java
+++ projects/twinCitiesTraffic/src/com/osilabs/android/apps/TCT.java
@@ -2,20 +2,37 @@
  * TCT.java
  */
 package com.osilabs.android.apps;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import com.osilabs.android.apps.R;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 /**
  * Main class
@@ -44,6 +61,8 @@ public class TCT extends Activity {
 	//
 	protected static String CURRENT_WEBVIEW_URL = TRAFFIC_MAP_URL;
 	protected static String CURRENT_TARGET = "traffic";
+	
+	Spinner spViewChoices;
 	WebView wvMain;
 
     @Override
@@ -51,20 +70,94 @@ public class TCT extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        wvMain = (WebView) findViewById(R.id.mainWebView);
+        //
+		// Quick View Spinner
+		// 
+        spViewChoices = (Spinner) findViewById(R.id.view_choice_spinner);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+			this, R.array.view_choices, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spViewChoices.setOnItemSelectedListener(new QuickViewOnItemSelectedListener());
+		spViewChoices.setAdapter(adapter);
+
+		//
+		// Web View
+		//
+		wvMain = (WebView) findViewById(R.id.mainWebView);
         WebSettings webSettings = wvMain.getSettings();
         webSettings.setSavePassword(false);
         webSettings.setSaveFormData(false);
-        //webSettings.setSupportZoom(true);
+        webSettings.setSupportZoom(true);
         webSettings.setDefaultFontSize(23);
         webSettings.setJavaScriptEnabled(true);
-        
+
         wvMain.setWebViewClient(new MyWebViewClient(this));
         wvMain.setWebChromeClient(new WebChromeClient());
-
         wvMain.loadUrl(CURRENT_WEBVIEW_URL);
     }
+
+
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
+        }
+    }
     
+    //
+	// Quick View Spinner
+	// 
+	public class QuickViewOnItemSelectedListener implements OnItemSelectedListener {
+	
+		public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+			// Toast.makeText(	parent.getContext(), 
+			//				"Loading: " + parent.getItemAtPosition(pos).toString(), 
+			//				Toast.LENGTH_LONG).show();
+			switch (pos) {
+		    case MENU_TRAFFIC:
+		    	// Toast.makeText(parent.getContext(), "Loading traffic", Toast.LENGTH_SHORT).show();
+		    	CURRENT_TARGET = "traffic";
+		    	if( CURRENT_WEBVIEW_URL == TRAFFIC_MAP_URL) {
+		    		wvMain.loadUrl("javascript: jumpTo('"+CURRENT_TARGET+"')");
+		    	} else {
+		    		CURRENT_WEBVIEW_URL = TRAFFIC_MAP_URL;
+		    		wvMain.loadUrl(CURRENT_WEBVIEW_URL+"?target="+CURRENT_TARGET);
+		    	}
+		    	break;
+	    		
+		    case MENU_INCIDENTS:
+		    	// Toast.makeText(parent.getContext(), "Loading incidents", Toast.LENGTH_SHORT).show();
+		    	CURRENT_TARGET = "incidents";
+		    	if( CURRENT_WEBVIEW_URL == TRAFFIC_MAP_URL) {
+		    		wvMain.loadUrl("javascript: jumpTo('"+CURRENT_TARGET+"')");
+		    	} else {
+		    		CURRENT_WEBVIEW_URL = TRAFFIC_MAP_URL;
+		    		wvMain.loadUrl(CURRENT_WEBVIEW_URL+"?target="+CURRENT_TARGET);
+		    	}
+		    	break;
+		        
+		    case MENU_ALERTS:
+		    	// Toast.makeText(parent.getContext(), "Loading alerts", Toast.LENGTH_SHORT).show();
+		    	CURRENT_TARGET = "alerts";
+		    	if( CURRENT_WEBVIEW_URL == TRAFFIC_MAP_URL) {
+		    		wvMain.loadUrl("javascript: jumpTo('"+CURRENT_TARGET+"')");
+		    	} else {
+		    		CURRENT_WEBVIEW_URL = TRAFFIC_MAP_URL;
+		    		wvMain.loadUrl(CURRENT_WEBVIEW_URL+"?target="+CURRENT_TARGET);
+		    	}
+		    	break;
+			}
+		}
+	
+		public void onNothingSelected(AdapterView parent) {
+		  // Do nothing.
+		}
+	}
+	
 	private class MyWebViewClient extends WebViewClient {
 		Activity activity;
 		public MyWebViewClient(Activity a) { activity = a; }
@@ -93,62 +186,23 @@ public class TCT extends Activity {
     
 	/* Creates the menu items */
 	public boolean onCreateOptionsMenu(Menu menu) {
-	    menu.add(0, MENU_TRAFFIC, 0, "Traffic");
-	    menu.add(0, MENU_INCIDENTS, 0, "Incidents");
-	    menu.add(0, MENU_ALERTS, 0, "Alerts");
 	    menu.add(0, MENU_REFRESH, 0, "Refresh");
-	    menu.add(0, MENU_QUIT, 0, "Quit");
 	    menu.add(0, MENU_MNDOT_MOBILE_FREEWAYS, 0, "Cameras");
+	    menu.add(0, MENU_QUIT, 0, "Quit");
 	    
 	    return true;
 	}
 	
 	/* Handles item selections */
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Get a context for toast
-		Context context = getApplicationContext();
-		
 	    switch (item.getItemId()) {
 		    case MENU_REFRESH:
-		    	Toast.makeText(context, "Refreshing", Toast.LENGTH_SHORT).show();
+		    	Toast.makeText(getApplicationContext(), "Refreshing", Toast.LENGTH_SHORT).show();
 		    	wvMain.loadUrl(CURRENT_WEBVIEW_URL+"?target="+CURRENT_TARGET);
 		        return true;
 		        
-		    case MENU_TRAFFIC:
-		    	Toast.makeText(context, "Loading traffic", Toast.LENGTH_SHORT).show();
-		    	CURRENT_TARGET = "traffic";
-		    	if( CURRENT_WEBVIEW_URL == TRAFFIC_MAP_URL) {
-		    		wvMain.loadUrl("javascript: jumpTo('"+CURRENT_TARGET+"')");
-		    	} else {
-		    		CURRENT_WEBVIEW_URL = TRAFFIC_MAP_URL;
-		    		wvMain.loadUrl(CURRENT_WEBVIEW_URL+"?target="+CURRENT_TARGET);
-		    	}
-	    		return true;
-	    		
-		    case MENU_INCIDENTS:
-		    	Toast.makeText(context, "Loading incidents", Toast.LENGTH_SHORT).show();
-		    	CURRENT_TARGET = "incidents";
-		    	if( CURRENT_WEBVIEW_URL == TRAFFIC_MAP_URL) {
-		    		wvMain.loadUrl("javascript: jumpTo('"+CURRENT_TARGET+"')");
-		    	} else {
-		    		CURRENT_WEBVIEW_URL = TRAFFIC_MAP_URL;
-		    		wvMain.loadUrl(CURRENT_WEBVIEW_URL+"?target="+CURRENT_TARGET);
-		    	}
-		        return true;
-		        
-		    case MENU_ALERTS:
-		    	Toast.makeText(context, "Loading alerts", Toast.LENGTH_SHORT).show();
-		    	CURRENT_TARGET = "alerts";
-		    	if( CURRENT_WEBVIEW_URL == TRAFFIC_MAP_URL) {
-		    		wvMain.loadUrl("javascript: jumpTo('"+CURRENT_TARGET+"')");
-		    	} else {
-		    		CURRENT_WEBVIEW_URL = TRAFFIC_MAP_URL;
-		    		wvMain.loadUrl(CURRENT_WEBVIEW_URL+"?target="+CURRENT_TARGET);
-		    	}
-		        return true;
-		        
 		    case MENU_MNDOT_MOBILE_FREEWAYS:
-		    	Toast.makeText(context, "Loading cameras", Toast.LENGTH_SHORT).show();
+		    	Toast.makeText(getApplicationContext(), "Loading cameras", Toast.LENGTH_SHORT).show();
 		    	CURRENT_WEBVIEW_URL = MNDOT_MOBILE_URL;
 		    	wvMain.loadUrl(CURRENT_WEBVIEW_URL);
 	    		return true;
