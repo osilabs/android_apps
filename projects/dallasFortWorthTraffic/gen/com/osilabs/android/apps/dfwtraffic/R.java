f37f230e5698286d077991c1338c3652b1dd249c
D. DeZurik <damien@osilabs.com>
2010-11-04 00:58:07 -0500

    DFWT v0.2 - RELEASE 20101104

--- projects/dallasFortWorthTraffic/gen/com/osilabs/android/apps/dfwtraffic/R.java
+++ projects/dallasFortWorthTraffic/gen/com/osilabs/android/apps/dfwtraffic/R.java
@@ -8,6 +8,11 @@
 package com.osilabs.android.apps.dfwtraffic;
 
 public final class R {
+    public static final class array {
+        public static final int pref_qview_option_values=0x7f040001;
+        public static final int pref_qview_options=0x7f040000;
+        public static final int view_choices=0x7f040002;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -15,12 +20,29 @@ public final class R {
         public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int mainImageView=0x7f050000;
+        public static final int mainWebView=0x7f070000;
+        public static final int view_choice_spinner=0x7f070001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int app_name=0x7f050000;
+        public static final int pref_current_view=0x7f05000b;
+        public static final int pref_default=0x7f050005;
+        public static final int pref_dialog_title=0x7f050009;
+        public static final int pref_list_summary=0x7f050008;
+        public static final int pref_list_title=0x7f050007;
+        public static final int pref_qview=0x7f050003;
+        public static final int pref_selected=0x7f05000a;
+        public static final int pref_summary=0x7f050006;
+        public static final int pref_title=0x7f050004;
+        /**  Preferences strings 
+         */
+        public static final int prefs_app_name=0x7f050002;
+        public static final int quick_view_label=0x7f050001;
+    }
+    public static final class style {
+        public static final int osistyle=0x7f060000;
     }
 }
