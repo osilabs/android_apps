697a75766898d00974fa815ed3243639cdf93094
D. DeZurik <damien@osilabs.com>
2010-11-06 00:52:33 -0500

    CA v0.0.3 - Knows red, green, blue, purple, black, white, gray.

---
+++ projects/colorblindAssist/src/com/osilabs/android/apps/colorblindassist/ColorDrop.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.osilabs.android.apps.colorblindassist;
+
+public class ColorDrop {
+	public int R = 0;
+	public int G = 0;
+	public int B = 0;
+	public int A = 0; // alpha
+	public int RGBint = 0;
+	public int bufallocsize = 0;
+	public int[] decodeBuf = {};
+	public String RGBstr = "0,0,0";
+	public String RGBdisplay = "rgb("+this.RGBstr+")";
+	public String hexval = "000000";
+	public String colorname = "";
+}
