e7cf4810525605177a2dd1232e8f72b27f775e8c
D. DeZurik <androidgit@osilabs.com>
2012-08-12 23:20:36 -0500

    CBA - Move ColorDrop to libOsiColor

--- projects/libOsiColor/src/com/osilabs/android/lib/osicolor/ColorDrop.java
+++ projects/libOsiColor/src/com/osilabs/android/lib/osicolor/ColorDrop.java
@@ -4,26 +4,28 @@
 package com.osilabs.android.lib.osicolor;
 
 public class ColorDrop {
-	public int R = 0;
-	public int G = 0;
-	public int B = 0;
-	public int A = 0; // alpha
-	public int RGBint = 0;
-	public int bufallocsize = 0;
-	public int[] decodeBuf = {};
-	public String RGBstr = "0,0,0";
-	public String RGBdisplay = "rgb("+this.RGBstr+")";
-	public String hexval = "000000";
-	public String colorname = "";
-	
-	/**
-	 * Sets members based on rgb string
-	 * @param rgb i.e. "60,255,63"
-	 */
-	public void setRGB(String rgb) {
-		String[] toks = rgb.split(",");
-		this.R = Integer.parseInt(toks[0]);
-		this.G = Integer.parseInt(toks[1]);
-		this.B = Integer.parseInt(toks[2]);
-	}
+    public int R = 0;
+    public int G = 0;
+    public int B = 0;
+    public int A = 0; // alpha
+    public int RGBint = 0;
+    public int bufallocsize = 0;
+    public int[] decodeBuf = {};
+    public String RGBstr = "0,0,0";
+    public String RGBdisplay = "rgb(" + this.RGBstr + ")";
+    public String hexval = "000000";
+    public String colorname = "";
+
+    /**
+     * Sets members based on rgb string
+     * 
+     * @param rgb
+     *            i.e. "60,255,63"
+     */
+    public void setRGB(String rgb) {
+        String[] toks = rgb.split(",");
+        this.R = Integer.parseInt(toks[0]);
+        this.G = Integer.parseInt(toks[1]);
+        this.B = Integer.parseInt(toks[2]);
+    }
 }
